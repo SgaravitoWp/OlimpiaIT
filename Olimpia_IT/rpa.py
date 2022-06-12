f64dcb5dd436bb0dea4ea3c35f1516d419c4a58c
SgaravitoWp <sgaravito@unal.edu.co>
2022-06-12 10:55:54 -0500

    API

--- Olimpia_IT/rpa.py
+++ Olimpia_IT/rpa.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.options import Options
-#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from config import variables
@@ -32,10 +32,10 @@ class Rpa():
                 self.info =  {"message" : "Error connecting to the page"}
             else: 
                 options = Options()
-                #options.add_argument('--headless')
+                options.add_argument('--headless')
                 options.add_argument('--disable-gpu')
-                #driver = webdriver.Chrome(options = options, executable_path = variables["FILE"] + "/config/chromedriver.exe")
-                driver = webdriver.Firefox(options = options, executable_path = variables["FILE"] + "/config/geckodriver.exe")
+                driver = webdriver.Chrome(options = options, executable_path = variables["FILE"] + "/config/chromedriver.exe")
+                #driver = webdriver.Firefox(options = options, executable_path = variables["FILE"] + "/config/geckodriver.exe")
                 driver.get("https://www.einforma.co/buscador-empresas-empresarios")
                 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
                 input =  driver.find_element(by=By.ID, value= "search2")
@@ -48,7 +48,7 @@ class Rpa():
                 else:
                     self.recursiveSearch(driver)
         except:
-            self.info = {"message" : "There are problems with the page. Please try again or try again later."}
+            self.info["message"] = "An error has ocurred during the process. Please, try again."
             try:
                 driver.close()
             except:
@@ -56,23 +56,26 @@ class Rpa():
 
     def recursiveSearch(self,driver):
 
-        WebDriverWait(driver, self.wait).until(EC.presence_of_element_located((By.XPATH, "//*[@id='a_nacional']")))
-        pre_score = driver.find_element(by=By.XPATH, value = "//*[@id='a_nacional']")
-        results_number = re.findall("Empresas y Empresarios \(([0-9]*)\)", pre_score.text)[0]
+        driver.find_element(by=By.XPATH, value= f"//*[@id='PROVINCIA']").click()
 
-        if int(results_number) == 0: 
-            self.info = {"message": "No results"} 
-            driver.close()
+        try:
+            driver.find_element(by=By.XPATH, value= f"//*[@id='PROVINCIA']/option[contains(text(), '{self.depto}')]").click()
+        except:
+            self.info = {"message": "ND"}
         else:
-            self.info["score"] = int(results_number)
-            self.info["results"] = list()
+            WebDriverWait(driver, self.wait).until(EC.presence_of_element_located((By.XPATH, "//*[@id='a_nacional']")))
+            pre_score = driver.find_element(by=By.XPATH, value = "//*[@id='a_nacional']")
+            results_number = re.findall("Empresas y Empresarios \(([0-9]*)\)", pre_score.text)[0]
 
-            self.switchSearch(driver)
+            if int(results_number) == 0: 
+                self.info = {"message": "No results"} 
+                driver.close()
+            else:
+                self.info["score"] = int(results_number)
+                self.info["results"] = list()
+
+                self.switchSearch(driver)
             
-            try:
-                if self.info["message"] == "ND":
-                    pass
-            except:
                 if self.counter == 12 or self.counter == 24:
                     driver.find_element(by=By.XPATH, value= f"//*[@id='nacional']/div[3]/div/div[2]/ul/li[{int(((self.counter)/12)+2)}]/a").click()
                     self.recursiveSearch(driver)
@@ -89,53 +92,48 @@ class Rpa():
         
         table_rows_results =  driver.find_elements(by=By.XPATH, value= "//*[@id='nacional']/tbody/tr")
 
+    
         for n in range(1, len(table_rows_results)+1):
 
             if n == 1:
                 self.driverFolderCreation()
 
-            row = driver.find_element(by=By.XPATH, value= f"//*[@id='nacional']/tbody/tr[{n}]/td[1]")
-            
+            row = driver.find_element(by=By.XPATH, value= f"//*[@id='nacional']/tbody/tr[{n}]/td[1]")   
+                                                        
+
             row.click()
 
             table_rows_resume =  driver.find_elements(by=By.XPATH, value= "//*[@id='imprimir']/table/tbody/tr")
             
-            driver.find_element(by=By.XPATH, value= f"//*[@id='PROVINCIA']").click()
-
-            try:
-                driver.find_element(by=By.XPATH, value= f"//*[@id='PROVINCIA']/option[contains(text(), '{self.depto}')]").click()
-            except:
-                self.info = {"message": "ND"}
-            else:
-                info_empresa = dict()
-                for m in range(2, len(table_rows_resume)+1):    
-                    try:
-                        driver.find_element(by=By.XPATH, value= f"/html/body/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/table")    
-                    except:
-                        basic_info_row = lambda x:  driver.find_element(by=By.XPATH, value= f"/html/body/div[2]/div[2]/div/div/div[2]/div/div[1]/div[3]/table/tbody/tr[{m}]/td[{x}]")     
-                    else:
-                        basic_info_row = lambda x:  driver.find_element(by=By.XPATH, value= f"/html/body/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/table/tbody/tr[{m}]/td[{x}]")
-                                            
-                    try:                                                           
-                        info_empresa[basic_info_row(1).text[:-1]] = basic_info_row(2).text
-                    except:
-                        continue
-            
-                row_name = info_empresa["Razón Social"].replace(" ", "-")
-
-                self.driverScreenshot(driver,row_name)
-
-                info_empresa["Search Number"] = self.counter + 1
-                info_empresa["ScreenShot"] = self.screenShotRoute(row_name) 
-                self.info["results"].append(info_empresa)
-                driver.back()
-
-                self.counter += 1
-
-                if (self.counter + 1 > self.number) or (self.counter + 1 > 30) :
-                    break
-                else:
+            info_empresa = dict()
+            for m in range(2, len(table_rows_resume)+1):    
+                try:
+                    driver.find_element(by=By.XPATH, value= f"/html/body/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/table")    
+                except:
+                    basic_info_row = lambda x:  driver.find_element(by=By.XPATH, value= f"/html/body/div[2]/div[2]/div/div/div[2]/div/div[1]/div[3]/table/tbody/tr[{m}]/td[{x}]")     
+                else:                                                                     
+                    basic_info_row = lambda x:  driver.find_element(by=By.XPATH, value= f"/html/body/div[2]/div[2]/div/div/div[2]/div/div[1]/div[2]/table/tbody/tr[{m}]/td[{x}]")
+                                        
+                try:                                                           
+                    info_empresa[basic_info_row(1).text[:-1]] = basic_info_row(2).text
+                except:
                     continue
+        
+            row_name = info_empresa["Razón Social"].replace(" ", "-")
+
+            self.driverScreenshot(driver,row_name)
+
+            info_empresa["Search Number"] = self.counter + 1
+            info_empresa["ScreenShot"] = self.screenShotRoute(row_name) 
+            self.info["results"].append(info_empresa)
+            driver.back()
+
+            self.counter += 1
+
+            if (self.counter + 1 > self.number) or (self.counter + 1 > 30) :
+                break
+            else:
+                continue
 
     def driverScreenshot(self, driver,name):
 
--- Olimpia_IT/serialization.py
+++ Olimpia_IT/serialization.py
@@ -1,5 +1,6 @@
 from marshmallow import Schema, fields
 
 class InfoSchema(Schema):
+    message = fields.String(required=None)
     score = fields.Integer()
     results = fields.List(fields.Dict(keys=fields.String(), values=fields.String(), required=False))
